84d9b08a5c9598e8ac6b72f6bd306db86542ef5d
JaeHyunGround <gothddlek@naver.com>
2023-11-20 03:07:56 +0900

    🐛 fix main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.scss";
 const Header = () => {
     return (
         <div className='Header_container'>
-            {/* navigate 기능을 포함한 헤더를 만들어볼게요. */}
+            {/* <div className='Header_title'>REX's portfolio</div> */}
         </div>
     );
 };
