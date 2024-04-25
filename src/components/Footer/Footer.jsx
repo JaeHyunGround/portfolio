d8c24d5ebb52d1909845346828db4bbd109040cd
JaeHyunGround <gothddlek@naver.com>
2024-04-26 00:10:02 +0900

    ♻️ Footer 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-    return (
-        <main className='Footer'>Copyright © 2023. Web Front-end developer portfolio by Jaehyun Ahn</main>
-    );
+  return (
+    <main className="Footer">
+      Copyright 2024. Frontend developer portfolio by Jaehyun Ahn
+    </main>
+  );
 };
 
 export default Footer;
